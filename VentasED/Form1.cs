16ce243db05cbf4336aff8243d212dbe82c4b246
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-06-13 07:55:20 -0700

    Mostrando productos utilizando arreglos

--- VentasED/Form1.cs
+++ VentasED/Form1.cs
@@ -17,13 +17,27 @@ namespace VentasED
             sabritas.Descripcion = "Chetos Flaming hot";
             sabritas.ValorUnitario = 18.00m;
 
+
+
             MessageBox.Show(cocacola600.ToString());
             MessageBox.Show(sabritas.ToString());
         }
 
         private void btnProductosArreglo_Click(object sender, EventArgs e)
         {
+            Producto cocacola600 = new Producto();
+            cocacola600.Descripcion = "Coca cola 600";
+            cocacola600.ValorUnitario = 20.00m;
 
+            Producto sabritas = new Producto();
+            sabritas.Descripcion = "Chetos Flaming hot";
+            sabritas.ValorUnitario = 18.00m;
+
+            Producto[] productos = new Producto[] { cocacola600, sabritas };
+            foreach(Producto producto in productos) 
+            {
+                MessageBox.Show(producto.ToString());
+            }
         }
     }
 }
